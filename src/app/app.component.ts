b8a0ba444483f0c5cd687c0345eadf9226eab9ea
Rico <ekirschke@web.de>
2025-03-16 16:47:29 +0100

    add routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatIconModule, MatSidenavModule, MatToolbarModule],
+  imports: [MatIconModule, MatSidenavModule, MatToolbarModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
